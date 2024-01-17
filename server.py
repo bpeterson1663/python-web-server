bda1278de98566fa6e2346fa00520a7f3124a98f
Brady Peterson <bpeterson1663@gmail.com>
2024-01-17 08:01:32 -0600

    feat(routing): contact form submit routing

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,12 @@ def hello_world():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name +'.html')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thankyou')
+    else:
+        return "something went wrong"
