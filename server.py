bf0f3ab85f9217da4ab08fa54e13fe7626e9d220
Brady Peterson <bpeterson1663@gmail.com>
2024-01-17 07:47:49 -0600

    feat(routes): dynamic routing

--- server.py
+++ server.py
@@ -5,14 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/works')
-def works():
-    return render_template('works.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name +'.html')
