d5b9763e97a62c6ed9c09521c1ea9aefad6c6848
Brady Peterson <bpeterson1663@gmail.com>
2024-01-17 07:15:25 -0600

    feat(routing): dynamic routing

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+
+@app.route('/blog/<int:post_id>')
+def blog_id(post_id=None):
+    return render_template('blog.html', id=post_id)
+
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
