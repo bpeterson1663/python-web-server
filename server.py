78fd78093a8efe4f1c903efdf64f8ce6a82ce2a5
Brady Peterson <bpeterson1663@gmail.com>
2024-01-18 20:02:08 -0600

    feat(lint): fix pylint errors

--- server.py
+++ server.py
@@ -1,29 +1,35 @@
-from flask import Flask, render_template, request, redirect
+"""Module providing csv."""
 import csv
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
+    """Function render index"""
     return render_template('index.html')
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
+    """Function render html page."""
     return render_template(page_name +'.html')
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
+    """Function submit contact form."""
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
             write_to_csv(data)
             return redirect('thankyou')
-        except:
+        except RuntimeWarning:
             return 'did not save to database'
     else:
         return "something went wrong, try again later"
     
 def write_to_csv(data):
-    with open('database.csv', newline="", mode="a") as database:
+    """Function write data to csv file."""
+    with open('database.csv', newline="", mode="a", encoding="utf-8") as database:
         fieldnames = ["email", "subject", "message"]
         email = data["email"]
         subject = data["subject"]
